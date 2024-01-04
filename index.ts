c2c23077f8fd2ae7178d998de5fb64fbf5af4110
Louis <coleloui18@gmail.com>
2024-01-04 13:23:46 -0800

    adding interface

--- index.ts
+++ index.ts
@@ -2,4 +2,17 @@ import axios from 'axios';
 
 const URL = 'https://jsonplaceholder.typicode.com/posts/1'
 
-axios.get(URL).then(response => console.log(response.data))
+axios.get(URL).then((response) => {
+    const post = response.data
+    
+    const id = post.userId
+    const header = post.title
+    const content = post.body
+
+    console.log(`
+        User ID is ${id}
+        Title of post: ${header}
+        Post Content: ${content}
+    `);
+    
+})
