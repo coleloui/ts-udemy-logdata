63c575c4009ec352df3eb67cd11bdbd551532c17
Louis <coleloui18@gmail.com>
2024-01-05 11:39:11 -0800

    completing logdata

--- index.ts
+++ index.ts
@@ -1,24 +1,34 @@
 import axios from 'axios';
 
 interface IPost {
-    userId: number;
-    title: string;
-    body: string;
+  userId: number;
+  title: string;
+  body: string;
 }
 
-const URL = 'https://jsonplaceholder.typicode.com/posts/1'
+const URL = 'https://jsonplaceholder.typicode.com/posts/1';
 
 axios.get(URL).then((response) => {
-    const post = response.data as IPost
-    
-    const userId = post.userId
-    const title = post.title
-    const body = post.body
+  const post = response.data as IPost;
 
-    console.log(`
-        User ID is ${userId}
-        Title of post: ${title}
-        Post Content: ${body}
-    `);
-    
-})
+  const userId = post.userId;
+  const title = post.title;
+  const body = post.body;
+
+  printPost({ userId, title, body });
+  printPost2( userId, title, body );
+});
+
+const printPost = (data: IPost): void =>
+  console.log(`
+    User ID is ${data.userId}
+    Title of post: ${data.title}
+    Post Content: ${data.body}
+`);
+
+const printPost2 = (userId: number, title:string, body: string): void =>
+  console.log(`
+    User ID is ${userId}
+    Title of post: ${title}
+    Post Content: ${body}
+`);
